46775a1aa22827ac41bbf16592083d61cf3d1ab1
sunliuxuan <2353624057@qq.com>
2023-10-05 20:18:36 +0800

    Add New Insert_Sort Method to simplify calculation

--- Sorting/Insertion_Sort.c
+++ Sorting/Insertion_Sort.c
@@ -1,23 +1,52 @@
 #include <stdio.h>
 
-//插入排序函数(从小到大选择排序)
-void Insertion_sort(int arr[],int size)
+// 插入排序函数(从小到大选择排序)
+void Insertion_sort(int arr[], int size)
 {
-    for(int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
     {
         int end = i;
         int temp = arr[end];
-        while(end > 0)
+        while (end > 0)
         {
-            if(arr[end-1] > temp)
+            if (arr[end - 1] > temp)
             {
-                arr[end] = arr[end-1];
+                arr[end] = arr[end - 1];
                 end--;
             }
             else
                 break;
         }
         arr[end] = temp;
+    }
+}
+
+// 二分法插入排序
+void Binary_InsertSort(int arr[], int size)
+{
+    int i = 0;
+    for (int i = 0; i < size; i++)
+    {
+        int left = 0;
+        int right = i - 1;
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            if (arr[i] > arr[mid])
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+        int temp = arr[i];
+        for (int right = i; right > left; right--)
+        {
+            arr[right] = arr[right - 1];
+        }
+        arr[left] = temp;
     }
 }
 
@@ -38,6 +67,12 @@ int main()
 
     printf("输出的数组为：");
     Insertion_sort(arr, 10);
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d", arr[i]);
+    }
+    printf("\n");
+    Binary_InsertSort(arr, 10);
     for (int i = 0; i < 10; i++)
     {
         printf("%d", arr[i]);
