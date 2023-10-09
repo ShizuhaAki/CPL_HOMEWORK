379c817d8f733e58bbef3853b560561188fb20dc
sunliuxuan <2353624057@qq.com>
2023-10-09 23:07:40 +0800

    Modify standup

--- Homework_2/standup.c
+++ Homework_2/standup.c
@@ -10,11 +10,11 @@ int main()
     }
     int num = 0;      // num表示引进的社牛数量
     int count = a[0]; // count表示原班级内已经站起来的人数
-    for (int i = 0; i <= n; i++)
+    for (int i = 1; i <= n; i++)
     {
         if ((a[i] != 0) && (num + count < i))
         {
-            num += (i - count);
+            num += (i - count - num);
         }
         count += a[i];
     }
