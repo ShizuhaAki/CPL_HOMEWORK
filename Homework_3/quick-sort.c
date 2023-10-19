db12210150c3491e9bff029c27b3f14b7037d04c
sunliuxuan <2353624057@qq.com>
2023-10-19 19:56:16 +0800

    Modify quick-sort

--- Homework_3/quick-sort.c
+++ Homework_3/quick-sort.c
@@ -1,27 +1,39 @@
 #include <stdio.h>
 
-void swap(int a, int b)
-{
-    int t;
-    t = b;
-    b = a;
-    a = t;
-}
-
 void quicksort(int arr[], int k, int n)
 {
     int pivot = arr[k - 1];
     int l = 0, r = n - 1;
-    while(l < r)
+    while (l != r)
     {
-        while(arr[l] <= pivot)
+        while (arr[l] < pivot && l < r)
             l++;
-        while(arr[r] >= pivot)
+        while (arr[r] >= pivot && l < r)
             r--;
-        if(l < r)
+
+        if (l < r)
         {
-            swap(arr[l], arr[r]);
+            int t = arr[l];
+            arr[l] = arr[r];
+            arr[r] = t;
         }
+    }
+
+    int record = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == pivot)
+        {
+            record = i;
+            break;
+        }
+    }
+
+    if (arr[l] > arr[record] && l < record)
+    {
+        int h = arr[record];
+        arr[record] = arr[l];
+        arr[l] = h;
     }
 }
 
@@ -30,16 +42,16 @@ int main()
     int n, k;
     scanf("%d%d", &n, &k);
     int arr[1000005] = {0};
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
 
     quicksort(arr, k, n);
 
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        printf("%d", arr[i]);
+        printf("%d ", arr[i]);
     }
     return 0;
 }
